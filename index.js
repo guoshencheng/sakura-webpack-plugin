44a99f8ff3776086a24c9ddbc6343f188ebc31f5
guoshencheng <648772021@qq.com>
2017-07-03 17:13:18 +0800

    add single option

--- index.js
+++ index.js
@@ -6,21 +6,34 @@ var mkdirp = require('mkdirp');
 module.exports = function(opt) {
   const prefix = (opt && opt.prefix) || "";
   const filePath = (opt && opt.filePath) || cwd;
+  const single = opt && !!opt.single;
   return function() {
     this.plugin('done', function(stats) {
       var namedChunks = stats.compilation.namedChunks;
       var hash = stats.hash;
-      var resources = Object.keys(namedChunks).reduce((pre, key) => {
-        var chunk = namedChunks[key];
-        const styles = chunk.files.filter(item => /\.css$/.test(item)).map(item => prefix + item);
-        const javascripts = chunk.files.filter(item => /\.js$/.test(item)).map(item => prefix + item);
-        var resource = {
-          styles, javascripts
-        }
-        return Object.assign(pre, {
-          [key]: resource
-        })
-      }, { hash })
+      var resources 
+      if (single) {
+        resources = Object.keys(namedChunks).reduce((pre, key) => {
+          var chunk = namedChunks[key];
+          const styles = chunk.files.filter(item => /\.css$/.test(item)).map(item => prefix + item);
+          const javascripts = chunk.files.filter(item => /\.js$/.test(item)).map(item => prefix + item);
+          pre.styles = pre.styles.concat(styles);
+          pre.javascripts = pre.javascripts.concat(javascripts);
+          return pre;
+        }, { styles: [], javascripts: [], hash })
+      } else {
+        resources = Object.keys(namedChunks).reduce((pre, key) => {
+          var chunk = namedChunks[key];
+          const styles = chunk.files.filter(item => /\.css$/.test(item)).map(item => prefix + item);
+          const javascripts = chunk.files.filter(item => /\.js$/.test(item)).map(item => prefix + item);
+          var resource = {
+            styles, javascripts
+          }
+          return Object.assign(pre, {
+            [key]: resource
+          })
+        }, { hash })
+      }
       mkdirp(filePath, (err) => {
         if (err) {
           console.log(err);
